09554648b8a372f0ac26ce1b91eacc4183000dd6
Dexter <superdexter7@gmail.com>
2025-07-07 13:56:51 -0400

    Build Fix

--- app.go
+++ app.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"embed"
-	"github.com/boombuler/barcode"
-	"github.com/dgrijalva/jwt-go"
-	"github.com/gorilla/securecookie"
-	"github.com/ncruces/julianday"
-	"github.com/stretchr/testify"
-	"github.com/tetratelabs/wazero"
-	"golang.org/x/crypto/bcrypt" // use a real subpackage
-	"gopkg.in/yaml.v2"
+	_ "github.com/boombuler/barcode"
+	_ "github.com/dgrijalva/jwt-go"
+	_ "github.com/gorilla/securecookie"
+	_ "github.com/ncruces/julianday"
+	_ "github.com/prometheus/client_golang/prometheus" // use a real subpackage
+	_ "github.com/stretchr/testify"
+	_ "github.com/tetratelabs/wazero"
+	_ "golang.org/x/crypto/bcrypt" // use a real subpackage
+	_ "gopkg.in/yaml.v2"
 	"log"
 	"net/http"
 	router "verademo-go/src-app/routes"
@@ -25,7 +26,6 @@ var resources embed.FS
 var templates embed.FS
 
 func main() {
-
 	session.Configure(session.Session{Name: "verademo", SecretKey: "key"})
 	sqlite.OpenDB()
 	log.Print("\nStarting VerademoGO....")
